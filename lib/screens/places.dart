de6853591cc84a94f63e71e003343a8cc233f2ee
911samuel <abayizeraeaz@gmail.com>
2025-05-06 10:16:31 +0200

    add riverpord to manage state

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,19 +1,27 @@
+import 'package:favorite_places_app/providers/user_places.dart';
 import 'package:favorite_places_app/screens/add_place.dart';
 import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
         actions: [
-          IconButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => AddPlaceScreen(),),);
-          }, icon: Icon(Icons.add))
+          IconButton(
+            onPressed: () {
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (ctx) => AddPlaceScreen()));
+            },
+            icon: Icon(Icons.add),
+          ),
         ],
       ),
       body: PlacesList(places: []),
